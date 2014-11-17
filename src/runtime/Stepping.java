c319a8bea4c2cb3d2a8db2c5ebe379ce431d5099
Ben Bemis <bbemis@purdue.edu>
2014-11-16 22:56:47 -0500

    Created the Stepping Class

---
+++ src/runtime/Stepping.java
@@ -0,0 +1,48 @@
+package runtime;
+
+import java.util.ArrayList;
+
+import tools.Thing;
+
+/**
+ * This class accepts an ArrayList of things and is designed to Step through
+ * all the elements in the array in a separate thread
+ * 
+ * @author Benjamin Bemis
+ *
+ */
+public class Stepping implements Runnable{
+	
+	private ArrayList<Thing> things;
+	private boolean finished;
+	
+	public Stepping(ArrayList<Thing> things){
+		this.things = things;
+		this.finished = false;
+	}
+	
+	public boolean isFinished(){ return finished; }
+
+	@Override
+	public void run() {
+		
+		for (int bbb = 0; bbb < things.size(); bbb++)
+			((Thing) things.get(bbb)).Step();
+		
+		
+		for(int bbb = 0; bbb < things.size(); bbb++){
+			Thing thing = things.get(bbb);
+			if(thing.mask != null){
+				if(thing.mask.getRect().intersects( Driver.frame ) )
+					thing.setRenderBoolean(true);
+				else
+					thing.setRenderBoolean(false);	
+			}else if(thing.mask == null)
+				thing.setRenderBoolean(true);
+		}
+		
+		finished = true;
+		
+	}
+
+}
